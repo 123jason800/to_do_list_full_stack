79b2fb2f561ffc5d7813a5083637778c04c1faf0
root <root@DESKTOP-E81SO17.localdomain>
2021-02-07 22:33:38 -0800

    changes

---
+++ app/assets/javascripts/requests/tasks.js
@@ -0,0 +1,63 @@
+
+$.ajaxSetup({
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+var indexTasks = function (successCB,errorCB) {
+    var request = {
+      type: 'GET',
+      url: 'api/tasks?api_key=1',
+      success: successCB,
+      error: errorCB
+    }
+    $.ajax(request);
+  };
+
+
+  var postTask = function (content,successCB,errorCB) {
+   
+    var request = {
+      type: 'POST',
+      url: 'api/tasks?api_key=1',
+      data: {
+        task: {
+          content: content
+        }
+      },
+      success: successCB,
+      error: errorCB
+    }
+    $.ajax(request);
+  };
+
+  var updateTask = function(id,completed,successCB,errorCB) {
+    var request = {
+      type: 'PUT',
+      url: `api/tasks/${id}/${completed}?api_key=1`,
+      data: {
+        task: {
+         
+        }
+      },
+      success: successCB,
+      error: errorCB
+    }
+    $.ajax(request);
+  }
+
+
+  var deleteTask = function(id,successCB,errorCB) {
+    var request = {
+      type: 'DELETE',
+      url: `api/tasks/${id}?api_key=1`,
+      data: {
+        task: {
+         
+        }
+      },
+      success: successCB,
+      error: errorCB
+    }
+    $.ajax(request);
+  }
---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,104 @@
+
+var loadTask = function() {
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      var active = task.completed ? 'completed': 'active';
+
+      return "<div class='col-12 mb-3 p-2 border rounded task "+ active + "' data-id='" + task.id + "'> \
+      " + task.content + "\
+      <div class='options'><button class='circle-btn' id='markTask'> </button><button class='btn delete-btn btn-danger h-100'>Remove</button></div></div>";
+    })
+    ;
+    var category = $('.btn-group').find('.active').attr('id');
+    switch (category) {
+      case 'completed':
+        htmlString = htmlString.filter(function(e) {
+          return $(e).hasClass('completed');
+        });
+        break;
+      case 'active':
+        htmlString = htmlString.filter(function(e) {
+          return $(e).hasClass('active');
+        });
+        break;
+    }
+
+  
+    $('#tasks').html(htmlString);
+      // addClickEvent('.circle-btn','click');
+    $('.circle-btn').each(function(){
+      addClickEvent($(this),'click','updateTask');
+    });
+    // addClickEvent (#deletTask,'click);
+    $('.delete-btn').each(function(){
+      addClickEvent($(this),'click','deleteTask');
+     });
+  });
+
+}
+
+var addClickEvent = function(item,e,action) {
+  $(item).on(e,function() {
+    var id = $(this).parents('.task').data('id');
+  
+    switch(action) {
+      case 'updateTask':
+        $(this).parents('.task').hasClass('active') ? updateTask(id,'mark_complete') : updateTask(id,'mark_active');
+        break;
+      case 'deleteTask':
+        deleteTask(id);
+        break;
+    }
+
+    loadTask();
+  });
+}
+;
+$(document).on("turbolinks:load", function () {
+  if ($('.static_pages.index').length > 0) {
+       // Even Listener for Posting Task
+
+      
+       $("#postTask").on( "keydown", function(event) {
+        var content = $("#postTask").val();
+        if(event.key === 'Enter' && content.length > 0) {
+          postTask(content);
+          loadTask();
+          $("#postTask").val('');
+        }
+      }); 
+      
+      // Event Listener for categories.
+      $('.btn-category').each( function() {
+        $(this).on('click', function () {
+            if (!$(this).hasClass('active')) {
+              
+              $('.btn-category').closest('.active').removeClass('active');
+              $(this).addClass('active');
+              loadTask();
+
+              
+              
+            
+            }
+        })
+      });
+
+
+
+    
+    loadTask();
+
+   
+    
+
+    
+  }
+
+  
+  
+ 
+});
+
+
+
